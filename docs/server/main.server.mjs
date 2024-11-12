ff66a0b17f7d6d2b1ea0972108978e9e1bc4b8b8
sadiq <sadiqhd@gmail.com>
2024-11-12 04:48:06 +0300

    Save changes in docs before switching to staging

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GKZJGVGW.mjs";import"./chunk-FOTYU3S6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-AVGWOZFC.mjs";import"./chunk-3ODW3P3B.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
