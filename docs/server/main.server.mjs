9b08b91923e1daf9a3b28cf574263ede3fd2550e
sadiq <sadiqhd@gmail.com>
2024-11-12 04:59:39 +0300

    Update docs for GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,6 +1,2 @@
 import './polyfills.server.mjs';
-<<<<<<< HEAD
-import{a}from"./chunk-2DPYMR7E.mjs";import"./chunk-2JS2H73W.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
-=======
-import{a}from"./chunk-GKZJGVGW.mjs";import"./chunk-FOTYU3S6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
->>>>>>> 21382fb05c067fb948d6ca5e2f88dae5b15f3920
+import{a}from"./chunk-AVGWOZFC.mjs";import"./chunk-3ODW3P3B.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
