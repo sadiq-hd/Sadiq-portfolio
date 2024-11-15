55e881a3090463d31854f52fca9c450768c61f71
sadiq <sadiqhd@gmail.com>
2024-11-16 01:26:04 +0300

    fix baseHref configuration

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AVGWOZFC.mjs";import"./chunk-3ODW3P3B.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6GQSVODN.mjs";import"./chunk-M67QBD6Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
