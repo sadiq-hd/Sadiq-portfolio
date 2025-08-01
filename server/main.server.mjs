bcbdcbb299a1be76b3c421773ee2b464abd76340
sadiq <sadiqhd@gmail.com>
2025-08-01 22:44:51 +0300

    Updates

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LKHCVHJQ.mjs";import"./chunk-USIQG4QM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-X2SVS5U4.mjs";import"./chunk-2Z43WKCN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
